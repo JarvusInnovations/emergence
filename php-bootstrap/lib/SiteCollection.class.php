8395263882848c3ba6c3a169ea07527ca4584f1e
henry <henry@e0.emr.ge>
2012-04-14 01:54:05 -0400

    Changed max http body size to 20MB in the nginx config generator allowing files bigger than 1MB to be uploaded through WebDAV. Also added a small patch from the PHP bootstrap.

--- php-bootstrap/lib/SiteCollection.class.php
+++ php-bootstrap/lib/SiteCollection.class.php
@@ -445,7 +445,7 @@ class SiteCollection
 			static::$tableName
 			,$parentCollection ? $parentCollection->Site : ($remote ? 'Remote' : 'Local')
 			,DB::escape($handle)
-			,!empty($GLOBALS['Session']) ? $GLOBALS['Session']->PersonID : null
+			,$GLOBALS['Session']->PersonID
 			,$parentCollection ? $parentCollection->ID : 'NULL'
 			,$left
 			,$right
