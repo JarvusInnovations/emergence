e1c2007610acc7cdc2a5800b130d18f02e124eed
Chris Alfano <chris@jarv.us>
2014-04-22 00:22:08 -0400

    return null for getByID instead of invalid instance

--- php-bootstrap/lib/SiteCollection.class.php
+++ php-bootstrap/lib/SiteCollection.class.php
@@ -78,7 +78,7 @@ class SiteCollection
     public static function getByID($collectionID)
     {
         $record = DB::oneRecord('SELECT * FROM `%s` WHERE ID = %u', array(static::$tableName, $collectionID));
-        return new static($record['Handle'], $record);
+        return $record ? new static($record['Handle'], $record) : null;
     }
 
     public static function getRecordByHandle($handle, $parentID = null, $remote = false, $includeDeleted = false)
