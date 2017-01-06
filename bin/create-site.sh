00c980f8058e1abb144ef6a0f6172c9ee4f905e0
Chris Alfano <chris@jarv.us>
2017-01-06 13:37:26 +0000

    Add --precache-trees, --generate-tables, --shell, and --verbose options to create-site

--- bin/create-site.sh
+++ bin/create-site.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DEFAULT_PRECACHE_TREES='dwoo-plugins,event-handlers,html-templates,php-classes,php-config,php-migrations,site-root,site-tasks'
+DEFAULT_GENERATE_TABLES='UserSession,User'
 
 for i in "$@"
 do
@@ -40,6 +42,34 @@ case $i in
         PARENT_KEY="${i#*=}"
         shift
         ;;
+    --precache-trees)
+        PRECACHE_TREES="$DEFAULT_PRECACHE_TREES"
+        shift
+        ;;
+    --precache-trees+=*)
+        PRECACHE_TREES="$DEFAULT_PRECACHE_TREES,${i#*=}"
+        shift
+        ;;
+    --precache-trees=*)
+        PRECACHE_TREES="${i#*=}"
+        shift
+        ;;
+    --generate-tables)
+        GENERATE_TABLES="$DEFAULT_GENERATE_TABLES"
+        shift
+        ;;
+    --generate-tables+=*)
+        GENERATE_TABLES="$DEFAULT_GENERATE_TABLES,${i#*=}"
+        shift
+        ;;
+    --generate-tables=*)
+        GENERATE_TABLES="${i#*=}"
+        shift
+        ;;
+    --shell)
+        ENTER_SHELL=YES
+        shift
+        ;;
     --verbose)
         VERBOSE=YES
         shift
@@ -50,6 +80,10 @@ case $i in
     ;;
 esac
 done
+
+if [ "$GENERATE_TABLES" ] || [ "$PRECACHE_TREES" ] || [ "$ENTER_SHELL" ]; then
+    command -v underscore >/dev/null 2>&1 || { echo >&2 "underscore must be installed to use --precache-trees or --generate-tables"; exit 1; }
+fi
 
 if [ -z "$KERNEL_HOST" ]; then
     KERNEL_HOST="localhost"
@@ -84,12 +118,12 @@ if [ -z "$PARENT_HOSTNAME" ]; then
 fi
 
 if [ "$VERBOSE" ]; then
-    echo "HANDLE                = ${HANDLE}"
-    echo "LABEL                 = ${LABEL}"
-    echo "PRIMARY_HOSTNAME      = ${PRIMARY_HOSTNAME}"
-    echo "SECONDARY_HOSTNAMES   = ${SECONDARY_HOSTNAMES}"
-    echo "PARENT_HOSTNAME       = ${PARENT_HOSTNAME}"
-    echo "PARENT_KEY            = ${PARENT_KEY}"
+    echo >&2 "HANDLE                = ${HANDLE}"
+    echo >&2 "LABEL                 = ${LABEL}"
+    echo >&2 "PRIMARY_HOSTNAME      = ${PRIMARY_HOSTNAME}"
+    echo >&2 "SECONDARY_HOSTNAMES   = ${SECONDARY_HOSTNAMES}"
+    echo >&2 "PARENT_HOSTNAME       = ${PARENT_HOSTNAME}"
+    echo >&2 "PARENT_KEY            = ${PARENT_KEY}"
 fi
 
 read -d '' REQUEST_BODY << END_OF_BODY
@@ -113,3 +147,32 @@ RESPONSE=$(
 )
 
 echo "$RESPONSE"
+
+if [ -z "$PRECACHE_TREES" ] && [ -z "$GENERATE_TABLES" ] && [ -z "$ENTER_SHELL" ]; then
+    exit 0
+fi
+
+SITE_HANDLE=$(echo "${RESPONSE}" | underscore --outfmt=text extract data.handle)
+
+if [ "$PRECACHE_TREES" ]; then
+    emergence-shell $SITE_HANDLE 1>&2 << END_OF_PHP
+        foreach (preg_split('/[\s,]/', "$PRECACHE_TREES") AS \$tree) {
+            echo "Caching \$tree...".str_repeat(" ",25-strlen(\$tree));
+            echo Emergence_FS::cacheTree(\$tree)." files downloaded\n";
+        }
+END_OF_PHP
+fi
+
+if [ "$GENERATE_TABLES" ]; then
+    emergence-shell $SITE_HANDLE 1>&2 << END_OF_PHP
+        foreach (preg_split('/[\s,]/', "$GENERATE_TABLES") AS \$className) {
+            echo "Generating table for \$className...".str_repeat(" ",40-strlen(\$className));
+            echo \$className::\$tableName."\n";
+            DB::multiQuery(SQL::getCreateTable(\$className));
+        }
+END_OF_PHP
+fi
+
+if [ "$ENTER_SHELL" ]; then
+    exec emergence-shell $SITE_HANDLE
+fi
