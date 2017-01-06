30cab8e0d295e74d91d1a058617a4034807b7a88
Chris Alfano <chris@jarv.us>
2017-01-06 14:05:03 +0000

    Make kernel API available over unix socket

--- kernel-lib/server.js
+++ kernel-lib/server.js
@@ -23,6 +23,7 @@ exports.Server = function(paths, config) {
     me.options.sslKey = me.options.sslKey || null;
     me.options.sslCert = me.options.sslCert || null;
     me.options.staticDir = me.options.staticDir || path.resolve(__dirname, '../kernel-www');
+    me.options.socketPath = 'socketPath' in me.options ? me.options.socketPath : '/emergence/kernel.sock';
 
     // initialize state
 };
@@ -31,81 +32,102 @@ util.inherits(exports.Server, events.EventEmitter);
 
 
 exports.Server.prototype.start = function() {
-    var me = this,
-        http_auth = require('http-auth')({
-            authRealm: 'Emergence Node Management',
-            authFile: '/emergence/admins.htpasswd'
-        }),
-        protocol;
+    // create authenticator
+    this.httpAuth = require('http-auth')({
+        authRealm: 'Emergence Node Management',
+        authFile: '/emergence/admins.htpasswd'
+    });
 
     // create static fileserver
-    me.fileServer = new static.Server(me.options.staticDir);
+    this.fileServer = new static.Server(this.options.staticDir);
 
-    // start control server
-    if (me.options.sslKey && me.options.sslCert) {
-        require('https').createServer({
-            key: fs.readFileSync(me.options.sslKey),
-            cert: fs.readFileSync(me.options.sslCert)
-        }, _handleRequest).listen(me.options.port, me.options.host);
+    // listen on web port
+    if (this.options.sslKey && this.options.sslCert) {
+        this.webServer = require('https').createServer({
+            key: fs.readFileSync(this.options.sslKey),
+            cert: fs.readFileSync(this.options.sslCert)
+        }, this.handleWebRequest.bind(this)).listen(this.options.port, this.options.host);
 
-        protocol = 'https';
+        this.webProtocol = 'https';
     } else {
-        require('http').createServer(_handleRequest).listen(me.options.port, me.options.host);
+        this.webServer = require('http').createServer(this.handleWebRequest.bind(this)).listen(this.options.port, this.options.host);
 
-        protocol = 'http';
+        this.webProtocol = 'http';
     }
 
-
-    function _handleRequest(request, response) {
-
-        http_auth.apply(request, response, function(username) {
-            request.content = '';
-
-            request.addListener('data', function(chunk) {
-                request.content += chunk;
-            });
-
-            request.addListener('end', function() {
-                request.urlInfo = url.parse(request.url)
-                request.path = request.urlInfo.pathname.substr(1).split('/');
-                console.log(request.method+' '+request.url);
-
-                if (request.path[0] == 'server-config') {
-                    response.writeHead(200, {'Content-Type':'application/json'});
-                    response.end(JSON.stringify(me.options));
-                    return;
-                }
-
-                if (request.path[0] == 'package-info') {
-                    response.writeHead(200, {'Content-Type':'application/json'});
-                    response.end(JSON.stringify(require('../package.json')));
-                    return;
-                }
-
-                if (me.paths.hasOwnProperty(request.path[0])) {
-                    var result = me.paths[request.path[0]].handleRequest(request, response, me);
-
-                    if (result===false)  {
-                        response.writeHead(404);
-                        response.end();
-                    } else if (result !== true) {
-                        response.writeHead(200, {'Content-Type':'application/json'});
-                        response.end(JSON.stringify(result));
-                    }
-                } else {
-                    me.fileServer.serve(request, response);
-                }
-            });
-
-        });
-
+    // listen on unix socket
+    if (this.options.socketPath) {
+        this.socketServer = require('http').createServer(this.handleRequest.bind(this)).listen(this.options.socketPath);
+        fs.chmodSync(this.options.socketPath, '400');
     }
 
+    // clean up on exit
+    process.on('exit', this.close.bind(this));
+    process.on('SIGINT', this.close.bind(this));
+    process.on('uncaughtException', this.close.bind(this));
 
-    console.log('Management server listening on '+protocol+'://'+me.options.host+':'+me.options.port);
-
+    console.log('Management server listening on '+this.webProtocol+'://'+this.options.host+':'+this.options.port);
 };
 
 exports.createServer = function(paths, options) {
     return new exports.Server(paths, options);
+};
+
+
+exports.Server.prototype.handleWebRequest = function(request, response) {
+    this.httpAuth.apply(request, response, function(username) {
+        handleRequest(request, response);
+    });
+};
+
+exports.Server.prototype.handleRequest = function(request, response) {
+    var me = this;
+
+    request.content = '';
+
+    request.addListener('data', function(chunk) {
+        request.content += chunk;
+    });
+
+    request.addListener('end', function() {
+        request.urlInfo = url.parse(request.url)
+        request.path = request.urlInfo.pathname.substr(1).split('/');
+        console.log(request.method+' '+request.url);
+
+        if (request.path[0] == 'server-config') {
+            response.writeHead(200, {'Content-Type':'application/json'});
+            response.end(JSON.stringify(me.options));
+            return;
+        }
+
+        if (request.path[0] == 'package-info') {
+            response.writeHead(200, {'Content-Type':'application/json'});
+            response.end(JSON.stringify(require('../package.json')));
+            return;
+        }
+
+        if (me.paths.hasOwnProperty(request.path[0])) {
+            var result = me.paths[request.path[0]].handleRequest(request, response, me);
+
+            if (result===false)  {
+                response.writeHead(404);
+                response.end();
+            } else if (result !== true) {
+                response.writeHead(200, {'Content-Type':'application/json'});
+                response.end(JSON.stringify(result));
+            }
+        } else {
+            me.fileServer.serve(request, response);
+        }
+    });
+};
+
+exports.Server.prototype.close = function() {
+    if (this.webServer) {
+        this.webServer.close();
+    }
+
+    if (this.socketServer) {
+        this.socketServer.close();
+    }
 };
