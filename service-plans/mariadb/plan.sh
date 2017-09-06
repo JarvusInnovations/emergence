c912890fbd6212f543ad52a509b1e3bef4fe9eb9
Chris Alfano <chris@jarv.us>
2017-09-06 18:20:19 -0400

    WIP: Upgrade mariadb to 10.2

--- service-plans/mariadb/plan.sh
+++ service-plans/mariadb/plan.sh
@@ -1,11 +1,12 @@
 pkg_name=mariadb
 pkg_origin=emergence
-pkg_version=10.1.18
+pkg_version=10.2.6
 pkg_description="An open source monitoring software for networks and applications"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPL-2.0')
-pkg_source=http://ftp.hosteurope.de/mirror/archive.mariadb.org//${pkg_name}-${pkg_version}/source/${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=d7336907e9ff44496d6453f92526b25bd253638a64a051ca879f953499873b73
+pkg_source=https://github.com/MariaDB/server/archive/${pkg_name}-${pkg_version}.tar.gz
+pkg_shasum=64dc9152daee2b396828963298edc55f36ad1add65a1f19d3db5ff0962d46842
+pkg_dirname="server-${pkg_name}-${pkg_version}"
 pkg_deps=(core/ncurses core/gcc-libs core/zlib core/sed)
 pkg_build_deps=(core/gcc core/make core/coreutils core/cmake)
 pkg_bin_dirs=(bin)
@@ -32,11 +33,16 @@ do_build() {
             -DCMAKE_PREFIX_PATH="$(pkg_path_for core/ncurses)" \
             -DCMAKE_BUILD_TYPE=Release \
             -DWITH_READLINE=OFF
+
     make
+
+    return $?
 }
 
 do_install() {
+    attach
     make install
+    attach
     rm -rf "${pkg_prefix}/mysql-test"
     rm -rf "${pkg_prefix}/bin/mysql_client_test"
     rm -rf "${pkg_prefix}/bin/mysql_test"
