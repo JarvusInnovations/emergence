55cf956b9ec80f3e6681a99c82754b749cef621d
Chris Alfano <chris@jarv.us>
2018-01-01 19:24:53 -0500

    Improve error handling

--- kernel.js
+++ kernel.js
@@ -13,16 +13,24 @@ const habitatRequired = '>=0.50';
 
 
 // start up in async context
-(async function start () {
+start().catch(err => logger.error('Failed to start kernel:', err.message));
+
+
+async function start () {
 
     // check habitat version
     if (!await habitat.satisfiesVersion(habitatRequired)) {
-        throw `Habitat version must be ${habitatRequired}, reported version is ${await habitat.getVersion()}`;
+        throw new Error(`Habitat version must be ${habitatRequired}, reported version is ${await habitat.getVersion()}`);
     }
 
 
-    // ensure supervisor is installed
-    await habitat('pkg', 'install', 'core/hab-sup', { passthrough: true, wait: true });
+    // ensure supervisor is available
+    try {
+        const result = await habitat('svc', 'status');
+        debugger;
+    } catch (err) {
+        throw new Error('Habitat supervisor must be running, start it as a daemon first');
+    }
 
 
     // install services
@@ -33,4 +41,4 @@ const habitatRequired = '>=0.50';
     await habitat('svc', 'load', 'emergence/nginx', { force: true, group: 'emergence' }, { passthrough: true, wait: true });
     await habitat('svc', 'load', 'emergence/php5', { force: true, group: 'emergence' }, { passthrough: true, wait: true });
     await habitat('svc', 'load', 'emergence/mysql', { force: true, group: 'emergence' }, { passthrough: true, wait: true });
-})();
+}
