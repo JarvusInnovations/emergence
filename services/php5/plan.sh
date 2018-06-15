76d669cd590a80f5670b0ddbe9809967ab2ec1e4
Chris Alfano <chris@jarv.us>
2018-06-15 18:57:34 -0400

    Move php readline dep to runtime

--- services/php5/plan.sh
+++ services/php5/plan.sh
@@ -18,6 +18,7 @@ pkg_deps=(
   core/libjpeg-turbo
   core/libpng
   core/openssl
+  core/readline
   core/zlib
 )
 pkg_build_deps=(
@@ -26,7 +27,6 @@ pkg_build_deps=(
   core/gcc
   core/make
   core/re2c
-  core/readline
 )
 pkg_bin_dirs=(bin sbin)
 pkg_lib_dirs=(lib)
